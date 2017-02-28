8c0a69048032b18bbb4b8c57eca4adf5c978e1d0
abraham kwak <kwakdevelop@gmail.com>
2017-02-27 22:31:34 -0500

    script.js update

--- script.js
+++ script.js
@@ -1,52 +1,45 @@
-//an object is created which stores all applications main functions
 var todoList = {
   todos: [],
   addTodo: function(todoText) {
-    //the parameter pushes an object with 2 properties 
-    //where the parameter becomes the value for the property todoText
+    //by passing in an object into the push method's parameter we push an object into the todos array with 2 properties
     this.todos.push({
       todoText: todoText,
       completed: false
-    })
+    });
   },
   changeTodo: function(position, changeText) {
-    //grab position inside object using bracket notation and then it's todoText property and assign it with the changeText parameter
+    //the first argument is the the item in the arrays position and we grab it's todoText property and update it with the second argument which changes it.
     this.todos[position].todoText = changeText;
   },
   deleteTodo: function(position) {
-    //grab position parameter and always delete one item at a time
+    //using the splice method we pass the position argument to find which position the item in the todos array we are trying to delete. the second parameter is set to 1 because we are deleting one item at a time.
     this.todos.splice(position, 1);
   },
   toggleCompleted: function(position) {
-    //store the todos array position into to save typing
+    //store the todos items position into a new variable for ease of use and typing.
     var todo = this.todos[position];
-    
-    //if completed is false make it true and vice versa
+    //we use the bang operator to switch the completed property's true or false value back and forth.
     todo.completed = !todo.completed;
   },
   toggleAll: function() {
-    //store the length of items inside the todos array 
     var totalTodos = this.todos.length;
-    
-    //completed todos starts at 0
     var completedTodos = 0;
     
-    // Get number of completed todos.
+    // Get number of completed todos by using a for loop to go through each item in the todos array and check each one's completed property and if it's true we add in 1 to the completedTodos variable.
     for (var i = 0; i < totalTodos; i++) {
       if (this.todos[i].completed === true) {
         completedTodos++;
       }
     }
     
-    //we run an if statement to see if completedTodos is equal to 
-    //totalTodos then a for loop to see what each completed property is set to
-    // Case 1: If everything’s true, make everything false.
+    //if completedTodos equals totalTodos we check for case 1 otherwise we check for case 2
     if (completedTodos === totalTodos) {
+      // Case 1: If everything’s true, make everything false.
       for (var i = 0; i < totalTodos; i++) {
         this.todos[i].completed = false;
       }
-    // Case 2: Otherwise, make everything true.
     } else {
+      // Case 2: Otherwise, make everything true.
       for (var i = 0; i < totalTodos; i++) {
         this.todos[i].completed = true;
       }      
@@ -56,106 +49,80 @@ var todoList = {
 
 var handlers = {
   addTodo: function() {
-    //grab the input by it's id attribute and store them into a variable
+    //the document getElementById method allows us to grab the element with the id used in the DOM.
     var addTodoTextInput = document.getElementById('addTodoTextInput');
-    
-    //the addTodo function adds the input's value using it's value property
+    //using the function we add the value property of the element
     todoList.addTodo(addTodoTextInput.value);
-    
-    //set the inputs value to an empty string so that it clears it after adding values
+    //we set the value to an empty string here because we want it to clear after each use of the this function
     addTodoTextInput.value = '';
-    
-    //because our view object handles the displayTodos function now we can add
-    //in the view object's version of the displayTodos function and we add this 
-    //in the handlers object since we need to see the results in the UI now as 
-    //opposed to seeing it in the console. we can also delete the console's version
-    //of the displayTodos function everywhere else and also the displayTodos button
-    //from our HTML *look at previous versions if you need to see for reference
+    //the displayTodos function moved from the todoList object into the view object and since we no longer need to call this function from the console we add it in each handlers method.
     view.displayTodos();
   },
   changeTodo: function() {
-    //grab both inputs by their id attributes and store them into a variable
     var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
     var changeTodoTextInput = document.getElementById('changeTodoTextInput');
-    
-    //the changeTodo function takes both inputs values using their value and valueAsNumber properties
+    //we use valueAsNumber for a number input and value for a string input
     todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
-    
-    //set the inputs value to an empty string so that it clears after adding values
     changeTodoPositionInput.value = '';
     changeTodoTextInput.value = '';
     view.displayTodos();
   },
   deleteTodo: function() {
     var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-    
     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-    
     deleteTodoPositionInput.value = '';
     view.displayTodos();
   },
   toggleCompleted: function() {
     var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
-    
     todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
-    
     toggleCompletedPositionInput.value = '';
     view.displayTodos();
   },
   toggleAll: function() {
     todoList.toggleAll();
     view.displayTodos();
-  }
+  }  
 };
 
-//the object created below handles the output of the interaction on to the 
-//user interface as opposed to looking at the console for the output
 var view = {
   displayTodos: function() {
-    //grab a reference to the ul element in the DOM
+    //using the querySelector we grab a reference to the ul on the DOM.
     var todosUl = document.querySelector('ul');
-    
-    //clear the ul element of any previous inner HTML when the function is run again
+    //we need to clear the ul element each time we call the displayTodo and make sure it starts from zero and adds the correct number of items that should display.
     todosUl.innerHTML = '';
-    
-    //loops through the todoList objects todos property
     for (var i = 0; i < todoList.todos.length; i++) {
-      //create li elements
       var todoLi = document.createElement('li');
-      
-      //while optional creating the variable below saves typing, we store each
-      //item from the array that comes out from the for loop into the variable
+      //this saves us some typing time.
       var todo = todoList.todos[i];
-      
-      //create a variable with an empty string that will soon combine the 
-      //todoText and the completion marks
       var todoTextWithCompletion = '';
-      
-      //run an if/else statment to see if it was completed and have it 
-      //show with the todoText property of each array item that comes from 
-      //the for loop
+
       if (todo.completed === true) {
         todoTextWithCompletion = '(x) ' + todo.todoText;
       } else {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
       
-       //sets the todoLi textContent property to todoTextWithCompletion
+      //we are grabbing each items position from the for loop so that is why we are able to set the id property to i.
+      //id property unlike the class property is unique to each one so there is only one id of position i e.g. id='0', id='1' and so forth.
+      //we do this so that when we click on the delete button it will look at the id of the list item and make sure to delete the correct id position item.
+      todoLi.id = i;
       todoLi.textContent = todoTextWithCompletion;
-      
-      //append the li elements into the ul
+      //we append the delete button to each list item that gets created.
+      todoLi.appendChild(this.createDeleteButton());
+      //we append the list item to the ul. 
       todosUl.appendChild(todoLi);
     }
+  },
+  createDeleteButton: function() {
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    //because there will be mulitple delete buttons being created we use the className property and assign a class name to each delete button that's created.
+    deleteButton.className = 'deleteButton';
+    //we want to return the button so that we can use it elsewhere. 
+    return deleteButton;
   }
 };
-
-
-
-
-
-
-
-
 
 
 
